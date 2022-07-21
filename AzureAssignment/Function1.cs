1581ca48bd2c85566fc4229c6e667db21063d884
divyamjalota25 <divyamjalota25@gmail.com>
2022-07-21 19:37:50 +0530

    final commit

--- AzureAssignment/Function1.cs
+++ AzureAssignment/Function1.cs
@@ -17,6 +17,7 @@ using System.Collections.Generic;
 using Azure.Storage.Blobs;
 using JsonLogic.Net;
 
+
 namespace AzureAssignment
 {
     public static class Function1
@@ -26,7 +27,7 @@ namespace AzureAssignment
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string connectionString = @"BlobEndpoint=https://assignmentyoda.blob.core.windows.net/;SharedAccessSignature=sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-07-21T03:20:18Z&st=2022-07-20T19:20:18Z&spr=https&sig=WkGvh7Y7v57it8DrDBoQjdo0BfkNBDxP51NpeDUXsxQ%3D";
+            string connectionString = @"BlobEndpoint=https://assignmentyoda.blob.core.windows.net/;SharedAccessSignature=sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-07-21T19:33:45Z&st=2022-07-21T11:33:45Z&spr=https&sig=asT1v2ypPLxXUduSyG5bKtfwBJ7pVRBXQPrBq3PztQA%3D";
 
             string blobName = "output";
 
@@ -59,11 +60,21 @@ namespace AzureAssignment
                 }
             }
             sr.Close();
-            //foreach (Employee employee in employeeList)
+            //foreach (Employee e in employeeList)
             //{
-            //    Console.WriteLine(employee.SALARY);
+            //    string ser = JsonConvert.SerializeObject(e);
             //}
+            using FileStream fileStream = File.Create(downloadPath + "_final.json");
+            await System.Text.Json.JsonSerializer.SerializeAsync(fileStream,employeeList);
+            fileStream.Close();
 
+            
+            BlobContainerClient outputContainer = new BlobContainerClient(connectionString, containerName);
+            BlobClient outputBlob = outputContainer.GetBlobClient("output_final.json");
+            using FileStream uploadFileStream = File.OpenRead(downloadPath + "_final.json");
+
+            await outputBlob.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
 
 
             return new OkObjectResult("ok");
